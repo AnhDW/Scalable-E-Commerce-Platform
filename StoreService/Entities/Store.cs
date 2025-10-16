9f600a72435deb367236f152f069d0fa6ffb8e38
vu anh <vungocanh723@gmail.com>
2025-10-16 16:58:43 +0700

    update

---
+++ StoreService/Entities/Store.cs
@@ -0,0 +1,10 @@
+﻿namespace StoreService.Entities
+{
+    public class Store
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Address { get; set; }
+    }
+}
