f8873df57cb878248c13ec3a43fe78442b3fa797
vu anh <vungocanh723@gmail.com>
2025-10-17 16:43:39 +0700

    update

--- StoreService/Entities/Store.cs
+++ StoreService/Entities/Store.cs
@@ -2,9 +2,9 @@
 {
     public class Store
     {
-        public int Id { get; set; }
+        public string Id { get; set; } = Guid.NewGuid().ToString();
         public string Name { get; set; }
-        public string Description { get; set; }
-        public string Address { get; set; }
+        public string? Description { get; set; }
+        public string? Address { get; set; }
     }
 }
